1428c96355f1724fce8208fa82e42fd9c4e75dc5
Atar Mor <atarmor92@gmail.com>
2024-03-25 17:23:17 +0200

    Add contact index route

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -2,22 +2,24 @@ const Router = ReactRouterDOM.HashRouter
 const { Route, Routes } = ReactRouterDOM
 const { Provider } = ReactRedux
 
+import { ContactIndex } from "./pages/ContactIndex.jsx"
 import { HomePage } from "./pages/HomePage.jsx"
 
 
 export function RootCmp() {
   return (
-    // <Provider store={store}>
-    <Router>
-      <section className="app">
-        <main>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-          </Routes>
-        </main>
-      </section>
-    </Router>
-    // </Provider>
+    <Provider store={store}>
+      <Router>
+        <section className="app">
+          <main>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/contact" element={<ContactIndex />} />
+            </Routes>
+          </main>
+        </section>
+      </Router>
+    </Provider>
   )
 }
 
