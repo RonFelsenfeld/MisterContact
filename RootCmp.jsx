f5bc5b095cdf659eb7d42d56b4a6a0d1834a26f5
Ron Felsenfeld <ronfelsenfeld@gmail.com>
2024-03-25 18:18:57 +0200

    Add routes to contact edit

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -5,7 +5,9 @@ const { Provider } = ReactRedux
 import { ContactIndex } from "./pages/ContactIndex.jsx"
 import { HomePage } from "./pages/HomePage.jsx"
 import { ContactDetails } from './pages/ContactDetails.jsx'
+import { ContactEdit } from "./pages/ContactEdit.jsx"
 import { store } from "./store/store.js"
+
 import { AppHeader } from "./cmp/AppHeader.jsx"
 
 
@@ -14,12 +16,14 @@ export function RootCmp() {
     <Provider store={store}>
       <Router>
         <section className="app">
-          <AppHeader/>
+          <AppHeader />
           <main>
             <Routes>
               <Route path="/" element={<HomePage />} />
               <Route path="/contact" element={<ContactIndex />} />
               <Route path="/contact/:contactId" element={<ContactDetails />} />
+              <Route path="/contact/edit" element={<ContactEdit />} />
+              <Route path="/contact/edit/:contactId" element={<ContactEdit />} />
             </Routes>
           </main>
         </section>
